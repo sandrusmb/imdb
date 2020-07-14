42e90424de4ddfe6eb5006b31466c3c574fef4eb
sandrusmb <sandrusmb@gmail.com>
2020-07-14 17:20:08 +0200

    Clean project

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button, Container, Typography } from '@material-ui/core';
+
+function Home() {
+    return (
+        <div>
+            <Button color="primary">Hello World</Button>
+            <Container><Typography>Hola</Typography></Container>
+        </div>
+    )
+}
+
+export default Home;
