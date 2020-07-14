4c26304e93cfbd5382c3bccdfef21c3dd9541202
sandrusmb <sandrusmb@gmail.com>
2020-07-14 18:36:44 +0200

    Add Home component functionality and styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,43 @@
-import React from 'react';
-import { Button, Container, Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Container, Card, Grid, Typography, TextField, Button } from '@material-ui/core';
 
 function Home() {
+
+    const [searchText, setSearchText] = useState("");
+
+    const handleSeachTextChange = (ev) => {
+        setSearchText(ev.target.value);
+        console.log()
+    }
+
+    const handleCleanTextClick = (ev) => {
+        console.log(10);
+    }
+
+    const handleSeachTextClick = (ev) => {
+        console.log(11);
+    }
+
     return (
         <div>
-            <Button color="primary">Hello World</Button>
-            <Container><Typography>Hola</Typography></Container>
+            <Container>
+                <Card>
+                    <Grid container>
+                        <Grid>
+                            <Typography>Bienvenido</Typography>
+                        </Grid>
+                        <Grid><label>Icono</label></Grid>
+                    </Grid>
+                    <TextField
+                        value={searchText}
+                        placeholder="Buscar..."
+                        onChange={handleSeachTextChange} />
+                    <Grid>
+                        <Button variant="contained" onClick={handleCleanTextClick}>Limpiar</Button>
+                        <Button variant="contained" color="primary" size="large" onClick={handleSeachTextClick}>Buscar</Button>
+                    </Grid>
+                </Card>
+            </Container>
         </div>
     )
 }
